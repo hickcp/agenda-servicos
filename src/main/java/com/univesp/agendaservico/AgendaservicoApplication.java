24af7a88a7eff98fe00991f28fc9cd2f39bc78bb
hickcp <henrique_c_p@outlook.com>
2023-05-30 13:43:04 -0300

    first commit

---
+++ src/main/java/com/univesp/agendaservico/AgendaservicoApplication.java
@@ -0,0 +1,13 @@
+package com.univesp.agendaservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaservicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univesp/agendaservico/AgendaservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univesp.agendaservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
